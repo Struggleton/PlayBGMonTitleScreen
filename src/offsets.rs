e93af12ab0e05282e33798197a7abbe03f62a4e4
Struggleton <thatstrugglethough@gmail.com>
2024-03-31 00:20:28 -0400

    change offsets from mutable to consts

--- src/offsets.rs
+++ src/offsets.rs
@@ -1,3 +1,3 @@
-pub static mut PRESS_ANY_BUTTON_OFFSET: usize = 0x1bfaf50;
-pub static mut HOW_TO_PLAY_OFFSET: usize = 0x1bfac90;
-pub static mut TITLE_SCREEN_EXIT_OFFSET: usize = 0x1bfb290;
+pub const PRESS_ANY_BUTTON_OFFSET: usize = 0x1bfaf50;
+pub const HOW_TO_PLAY_OFFSET: usize = 0x1bfac90;
+pub const TITLE_SCREEN_EXIT_OFFSET: usize = 0x1bfb290;
