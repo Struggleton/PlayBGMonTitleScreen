7214d24b1d3e0f3a72ccf7ee6a1a40f4b9eb6679
Struggleton <thatstrugglethough@gmail.com>
2024-03-31 01:25:16 -0400

    run rustfmt

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-use serde::{Deserialize, Serialize};
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io;
 
@@ -7,51 +7,53 @@ pub const CONFIG_PATH: &str = "sd:/ultimate/config_title.toml";
 
 #[derive(Serialize, Deserialize)]
 pub struct TitleConfig {
-	pub ui_bgm_id: hash40::Hash40,
+    pub ui_bgm_id: hash40::Hash40,
 }
 
 impl TitleConfig {
-	pub fn new() -> TitleConfig {
-		TitleConfig {
-			ui_bgm_id: Result::expect(hash40::Hash40::from_label("ui_bgm_crs12_simple_result_final"), 
-									"Could not convert label to Hash40!")
-		}
-	}
-	
-	pub fn load() -> Result<TitleConfig> {
-		if fs::metadata(CONFIG_PATH).is_ok() {
-			let toml_str = fs::read_to_string(CONFIG_PATH)?;
-			let title_config = toml::from_str::<TitleConfig>(&toml_str)?;
-			Ok(title_config)
-		} else {
-			Err(io::Error::from(io::ErrorKind::NotFound).into())
-		}
-	}
+    pub fn new() -> TitleConfig {
+        TitleConfig {
+            ui_bgm_id: Result::expect(
+                hash40::Hash40::from_label("ui_bgm_crs12_simple_result_final"),
+                "Could not convert label to Hash40!",
+            ),
+        }
+    }
 
-	pub fn load_or_create() -> Result<TitleConfig> {
-		match TitleConfig::load() {
-			Ok(c) => Ok(c),
-			Err(e) => {
-				if e.is::<io::Error>()
-					&& e.downcast_ref::<io::Error>().unwrap().kind() == io::ErrorKind::NotFound
-				{
-					 // No config file exists already
-					 TitleConfig::create_default()?;
-					 TitleConfig::load()
-				} else if e.is::<toml::de::Error>() {
-					// A config file exists but its not in the right format
+    pub fn load() -> Result<TitleConfig> {
+        if fs::metadata(CONFIG_PATH).is_ok() {
+            let toml_str = fs::read_to_string(CONFIG_PATH)?;
+            let title_config = toml::from_str::<TitleConfig>(&toml_str)?;
+            Ok(title_config)
+        } else {
+            Err(io::Error::from(io::ErrorKind::NotFound).into())
+        }
+    }
+
+    pub fn load_or_create() -> Result<TitleConfig> {
+        match TitleConfig::load() {
+            Ok(c) => Ok(c),
+            Err(e) => {
+                if e.is::<io::Error>()
+                    && e.downcast_ref::<io::Error>().unwrap().kind() == io::ErrorKind::NotFound
+                {
+                    // No config file exists already
+                    TitleConfig::create_default()?;
+                    TitleConfig::load()
+                } else if e.is::<toml::de::Error>() {
+                    // A config file exists but its not in the right format
                     fs::remove_file(CONFIG_PATH)?;
                     TitleConfig::create_default()?;
                     TitleConfig::load()
-				} else {
+                } else {
                     // Some other error, re-raise it
                     Err(e)
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
-	 /// Creates a default config and saves to file
+    /// Creates a default config and saves to file
     /// Returns Err if the file already exists
     pub fn create_default() -> Result<()> {
         if fs::metadata(CONFIG_PATH).is_ok() {
@@ -62,6 +64,5 @@ impl TitleConfig {
             fs::write(CONFIG_PATH, contents)?;
             Ok(())
         }
-	}
+    }
 }
-	
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::sync::atomic::{self, AtomicBool};
 
-use skyline::{hook, install_hooks};
 use skyline::hooks::InlineCtx;
+use skyline::{hook, install_hooks};
 
 pub mod config;
 pub use config::*;
@@ -11,52 +11,50 @@ pub use offsets::*;
 static IS_PLAYING: AtomicBool = AtomicBool::new(false);
 
 // We are using a hook to a function for if the
-// game is in the title screen's "Press any button state." 
+// game is in the title screen's "Press any button state."
 #[hook(offset = offsets::PRESS_ANY_BUTTON_OFFSET, inline)]
 fn create_press_any_hook(ctx: &mut InlineCtx) {
-		// This function gets called multiple times, so
-		// we use the IS_PLAYING boolean to check if we're already
-		// Playing the bgm
-		if !(IS_PLAYING.load(atomic::Ordering::Relaxed)) {
-			// Load the TitleConfig file and get the hash we're
-			// supposed to play
-			let title_conf = TitleConfig::load_or_create();
-			match title_conf {
-				Ok(c) => {
-					the_csk_collection_api::play_bgm(c.ui_bgm_id.0);
-					IS_PLAYING.store(true, atomic::Ordering::Relaxed);
-				}
-				Err(_e) => {
-					// TO-DO find out how to raise error
-				}
-			}
-		}
-	}
+    // This function gets called multiple times, so
+    // we use the IS_PLAYING boolean to check if we're already
+    // Playing the bgm
+    if !(IS_PLAYING.load(atomic::Ordering::Relaxed)) {
+        // Load the TitleConfig file and get the hash we're
+        // supposed to play
+        let title_conf = TitleConfig::load_or_create();
+        match title_conf {
+            Ok(c) => {
+                the_csk_collection_api::play_bgm(c.ui_bgm_id.0);
+                IS_PLAYING.store(true, atomic::Ordering::Relaxed);
+            }
+            Err(_e) => {
+                // TO-DO find out how to raise error
+            }
+        }
+    }
+}
 
 // Hook for the "How to Play" state
 // We need to mute the audio, so play silent audio
 #[hook(offset = offsets::HOW_TO_PLAY_OFFSET, inline)]
 fn create_how_to_hook(ctx: &mut InlineCtx) {
-	
-		IS_PLAYING.store(false, atomic::Ordering::Relaxed);
-		let bgm_hash = Result::expect(hash40::Hash40::from_label("m21b_gaw_gamer_mom"), 
-												"Could not convert label to Hash40!");
-		the_csk_collection_api::play_bgm(bgm_hash.0); 
-	
+    IS_PLAYING.store(false, atomic::Ordering::Relaxed);
+    let bgm_hash = Result::expect(
+        hash40::Hash40::from_label("m21b_gaw_gamer_mom"),
+        "Could not convert label to Hash40!",
+    );
+    the_csk_collection_api::play_bgm(bgm_hash.0);
 }
 
 // Hook for leaving the title screen
 // Change the IS_PLAYING boolean
 #[hook(offset = offsets::TITLE_SCREEN_EXIT_OFFSET, inline)]
 fn create_exit_hook(ctx: &mut InlineCtx) {
-	// so that when we go back to the title screen
-	// it will start playing again.
-	IS_PLAYING.store(false, atomic::Ordering::Relaxed);
+    // so that when we go back to the title screen
+    // it will start playing again.
+    IS_PLAYING.store(false, atomic::Ordering::Relaxed);
 }
 
 #[skyline::main(name = "PlayBGMonTitleScreen")]
 pub fn main() {
-    install_hooks!(create_press_any_hook, 
-				   create_exit_hook, 
-				   create_how_to_hook,);
+    install_hooks!(create_press_any_hook, create_exit_hook, create_how_to_hook,);
 }
