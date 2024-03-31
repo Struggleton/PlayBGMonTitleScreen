10781d5dbb6228c43523da413fab1cac39f875af
Struggleton <thatstrugglethough@gmail.com>
2024-03-31 00:41:45 -0400

    switch to AtomicBool and remove unsafe code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::sync::atomic::{self, AtomicBool};
+
 use skyline::{hook, install_hooks};
 use skyline::hooks::InlineCtx;
 
@@ -6,24 +8,23 @@ pub use config::*;
 pub mod offsets;
 pub use offsets::*;
 
-static mut IS_PLAYING: bool = false;
+static IS_PLAYING: AtomicBool = AtomicBool::new(false);
 
 // We are using a hook to a function for if the
 // game is in the title screen's "Press any button state." 
 #[hook(offset = offsets::PRESS_ANY_BUTTON_OFFSET, inline)]
 fn create_press_any_hook(ctx: &mut InlineCtx) {
-	unsafe {
 		// This function gets called multiple times, so
 		// we use the IS_PLAYING boolean to check if we're already
 		// Playing the bgm
-		if !(IS_PLAYING) {
+		if !(IS_PLAYING.load(atomic::Ordering::Relaxed)) {
 			// Load the TitleConfig file and get the hash we're
 			// supposed to play
 			let title_conf = TitleConfig::load_or_create();
 			match title_conf {
 				Ok(c) => {
 					the_csk_collection_api::play_bgm(c.bgm_hash);
-					IS_PLAYING = true;
+					IS_PLAYING.store(true, atomic::Ordering::Relaxed);
 				}
 				Err(_e) => {
 					// TO-DO find out how to raise error
@@ -31,16 +32,15 @@ fn create_press_any_hook(ctx: &mut InlineCtx) {
 			}
 		}
 	}
-}
 
 // Hook for the "How to Play" state
 // We need to mute the audio, so play silent audio
 #[hook(offset = offsets::HOW_TO_PLAY_OFFSET, inline)]
 fn create_how_to_hook(ctx: &mut InlineCtx) {
-	unsafe {
-		IS_PLAYING = false;
+	
+		IS_PLAYING.store(false, atomic::Ordering::Relaxed);
 		the_csk_collection_api::play_bgm(0x12129dab28); 
-	}
+	
 }
 
 // Hook for leaving the title screen
@@ -49,9 +49,7 @@ fn create_how_to_hook(ctx: &mut InlineCtx) {
 fn create_exit_hook(ctx: &mut InlineCtx) {
 	// so that when we go back to the title screen
 	// it will start playing again.
-	unsafe {
-		IS_PLAYING = false;
-	}
+	IS_PLAYING.store(false, atomic::Ordering::Relaxed);
 }
 
 #[skyline::main(name = "PlayBGMonTitleScreen")]
